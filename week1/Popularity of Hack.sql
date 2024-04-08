9705177e3952d3de53c8e91428a03d3fa01a2fab
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-08 17:59:59 +0900

    Update Popularity of Hack.sql

--- week1/Popularity of Hack.sql
+++ week1/Popularity of Hack.sql
@@ -2,3 +2,16 @@ select a.location, avg(b.popularity) avg_popularity
 from facebook_employees a join facebook_hack_survey b
 on a.id = b.employee_id
 group by a.location
+
+--[제풀이]
+select location,
+avg(popularity) avg_popularity
+from facebook_employees as t1
+inner join facebook_hack_survey as t2
+on t1.id=t2.employee_id
+group by location;
+
+/*
+[피드백]
+제 풀이와 동일합니다:)
+*/
