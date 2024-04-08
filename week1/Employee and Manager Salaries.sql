8005610d7846a4e77c991cadb2c90a3a07b1516b
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-08 17:59:35 +0900

    Update Employee and Manager Salaries.sql

--- week1/Employee and Manager Salaries.sql
+++ week1/Employee and Manager Salaries.sql
@@ -5,3 +5,17 @@ from employee a join
     where department like ('Management')) b
 on a.manager_id = b.id
 where a.salary > b.salary
+
+
+--[제풀이]
+select e.first_name,
+e.salary
+from employee as e
+inner join employee as m -- self join
+on e.manager_id=m.id
+where e.salary > m.salary;
+
+/*
+[피드백]
+재영님 코드도 올바른 코드입니다. 하지만 self join으로 푼 제 코드도 확인 부탁드립니다.
+*/
