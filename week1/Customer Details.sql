57fb18436f0dfda243fb58e201b9497a39277ffe
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-08 17:59:04 +0900

    Update Customer Details.sql

--- week1/Customer Details.sql
+++ week1/Customer Details.sql
@@ -2,3 +2,19 @@ select a.first_name, a.last_name, a.city, b.order_details
 from customers a left outer join orders b
 on a.id = b.cust_id
 order by 1, 4;
+
+
+--[Gemma풀이]
+select first_name,
+last_name,
+city,
+order_details
+from customers as t1
+left outer join orders as t2
+on t1.id=t2.cust_id
+order by first_name, order_details;
+
+/*
+[피드백]
+제 풀이와 동일합니다:)
+*/
