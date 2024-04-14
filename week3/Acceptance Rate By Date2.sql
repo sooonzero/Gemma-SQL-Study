3b831f279d6e560843f7346da3d63d26c1158480
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-14 23:43:55 +0900

    Update Acceptance Rate By Date2.sql

--- week3/Acceptance Rate By Date2.sql
+++ week3/Acceptance Rate By Date2.sql
@@ -1,1 +1,15 @@
 # 예상 답안의 컬럼 이름인 count(a.user_id_receiver)/count(b.user_id_sender) 에 맞춰 답을 작성해보았다.
+
+select sent_date as date, count(a.user_id_receiver)/count(b.user_id_sender)
+from (
+    select user_id_sender, user_id_receiver, date as accepted_date
+    from fb_friend_requests
+    where action like ('accepted')
+)a right outer join (
+    select user_id_sender, user_id_receiver, date as sent_date
+    from fb_friend_requests
+    where action like ('sent')
+) b 
+on a.user_id_sender = b.user_id_sender
+group by sent_date
+
