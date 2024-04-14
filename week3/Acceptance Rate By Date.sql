5c27c07acb6381ca16153d257e4d59bba53ca6c4
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-14 23:23:14 +0900

    Update Acceptance Rate By Date.sql

--- week3/Acceptance Rate By Date.sql
+++ week3/Acceptance Rate By Date.sql
@@ -5,5 +5,5 @@ from (select date, case when max(action) != min(action) then 1 else 0 end cnt
 group by a.date
 order by 1;
  
-# 여기도 예상답안에 count(a.user_id_receiver)/count(b.user_id_sender) 로 컬럼값이 나와있었다.
+# 여기도 예상답안에 count(a.user_id_receiver)/count(b.user_id_sender) 로 컬럼이름이 나와있었다.
 # 컬럼값을 통해 답이 안나오도록 최대한 다른 방법을 고안해서 문제를 풀어보았다.
