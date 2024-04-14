d5142baf453267bde7098f145b674f53d8cddbc1
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-14 23:21:30 +0900

    Create Acceptance Rate By Date.sql

---
+++ week3/Acceptance Rate By Date.sql
@@ -0,0 +1,9 @@
+select a.date, sum(a.cnt)/count(a.cnt)
+from (select date, case when max(action) != min(action) then 1 else 0 end cnt
+    from fb_friend_requests
+    group by user_id_sender, user_id_receiver) a
+group by a.date
+order by 1;
+ 
+# 여기도 예상답안에 count(a.user_id_receiver)/count(b.user_id_sender) 로 컬럼값이 나와있었다.
+# 컬럼값을 통해 답이 안나오도록 최대한 다른 방법을 고안해서 문제를 풀어보았다.
