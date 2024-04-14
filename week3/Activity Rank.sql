18ff2fc6b73ed60cec220bf30a3f92e9aa4c8ef0
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-14 23:20:02 +0900

    Update Activity Rank.sql

--- week3/Activity Rank.sql
+++ week3/Activity Rank.sql
@@ -3,5 +3,5 @@ from google_gmail_emails
 group by from_user
 order by 3 asc;
 
--- ROW_NUMBER() 하는거 몰랐는데 예상출력 컬럼에 그대로 있어서 사용함
--- ROW_NUMBER()관련해서는 추 후 정리 예정
+# ROW_NUMBER() 하는거 몰랐는데 예상출력 컬럼에 그대로 있어서 사용함
+# ROW_NUMBER() 관련해서는 추 후 정리 예정
