1f4a55e36a65edd1f485ecd211488974cdea31bd
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-04-28 06:10:06 +0900

    Update User with Most Approved Flags.sql

--- week4/User with Most Approved Flags.sql
+++ week4/User with Most Approved Flags.sql
@@ -1,3 +1,4 @@
+[User with Most Approved Flags]
 select c.username
 from (
 select concat(b.user_firstname," ",b.user_lastname) username, rank() over(order by count(reviewed_outcome) desc) ranking
@@ -7,4 +8,7 @@ select concat(b.user_firstname," ",b.user_lastname) username, rank() over(order 
     ) c
 where c.ranking = 1
 
-#답이 다르다, 결국 못풀음
+'''
+reviewed_outcome이 APPROVED인 경우 횟수를 구해서 가장 높은 횟수를 지닌 username 출력
+Richard Hasson이 4번으로 1등 Mark May가 3번으로 2등이지만 Expected Output은 Mark May 와 Richard Hasson이 공동 1등
+'''
