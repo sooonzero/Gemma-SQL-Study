b0989aef4dfa4dc77223e190ab2eeb21f1ee6170
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-05-19 01:45:57 +0900

    Create Income By Title and Gender.sql

---
+++ May_week2/Income By Title and Gender.sql
@@ -0,0 +1,7 @@
+select a.employee_title, a.sex, sum(a.salary+b.bonus) / count(b.worker_ref_id)
+from sf_employee a left join (select worker_ref_id, sum(bonus) bonus
+                            from sf_bonus
+                            group by worker_ref_id) b
+on a.id = b.worker_ref_id
+where b.bonus is not null
+group by employee_title
