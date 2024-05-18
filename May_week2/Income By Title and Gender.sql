191267083ba3c12537091c551099ffd30a712acf
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-05-19 01:46:14 +0900

    Update Income By Title and Gender.sql

--- May_week2/Income By Title and Gender.sql
+++ May_week2/Income By Title and Gender.sql
@@ -1,4 +1,4 @@
-select a.employee_title, a.sex, sum(a.salary+b.bonus) / count(b.worker_ref_id)
+select a.employee_title, a.sex, sum(a.salary+b.bonus) / count(b.worker_ref_id) avg_compensation
 from sf_employee a left join (select worker_ref_id, sum(bonus) bonus
                             from sf_bonus
                             group by worker_ref_id) b
