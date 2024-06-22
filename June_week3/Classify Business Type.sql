139cf3d41cc5da6aa6580db94ab1686be5fbe7b4
이재영 <jyclub10@naver.com>
2024-06-22 19:04:59 +0900

    Create Classify Business Type.sql

---
+++ June_week3/Classify Business Type.sql
@@ -0,0 +1,6 @@
+select distinct(business_name),
+case when business_name like ('%restaurant%' ) then 'restaurant'
+     when business_name like ('%cafe%') or business_name like ('%café%') or business_name like ('%coffee%') then 'cafe'
+     when business_name like ('%school%') then 'school'
+     else 'other' end as business_type
+from sf_restaurant_health_violations;
